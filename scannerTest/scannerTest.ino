a1e4c26c0b64d6c80fe329c5c62c38cf5bd62db8
Gustav Olsson <gustav128@gmail.com>
2015-05-14 19:33:49 +0200

    The robot can now sometimes approach the puck correctly

--- scannerTest/scannerTest.ino
+++ scannerTest/scannerTest.ino
@@ -6,24 +6,24 @@ Servo servoRight;
 
 const int pingPinHigh = 6;
 const int pingPinLow = 7;
+const unsigned long sonarThreshold = 10; // cm
+const unsigned long sonarRange = 60; //cm
+const int sonarReadDelay = 20; // ms
 
-// Scan outputs
-boolean foundPuck = false;
+const int scanServoAngleOffset = 8;
+const int scanServoAngleLimit = 60; // degrees
+int scanServoAngle = 0; // degrees
+int scanServoAngleDelta = 5; // degrees
+
+bool sawThisSweep = false;
+bool seesPuck = false;
+bool sawPuckLastCheck = false;
+int puckStartAngle = 0;
+int puckEndAngle = 0;
+
+bool foundPuck = false;
 int foundPuckAngle = 0;
-boolean foundWall = false;
-int foundWallAngle = 0;
-// TODO:
-//int foundPuckDist = 0;
-//int foundWallDist = 0;
-
-//Scan internal variables
-boolean seesPuck = false; 
-int leftScanEdge = 0;
-int rightScanEdge = 0;
-boolean scanDirection = false; // true is right, false is left
-int scanServoAngle = 0; // higher is more left
-int dScanAngle = 25; // going left
-int scanRange = 600;
+int foundPuckStraightAheadTime = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -31,136 +31,113 @@ void setup() {
   servoScanner.attach(11);
   servoLeft.attach(13);
   servoRight.attach(12);
-  servoLeft.writeMicroseconds(1500);   // Set left servo speed
-  servoRight.writeMicroseconds(1500); // Set right servo speed
 }
 
 void loop() {
-  scanForPuck(pingPinLow, pingPinHigh);
-  //delay(25);
-  //unsigned int cmHigh = checkSonar(pingPinHigh);
-  //unsigned int cmLow = checkSonar(pingPinLow);
-  //Serial.print("   foundPuck: ");
- //Serial.print(foundPuck);
-//  Serial.print("   foundPuckAngle: ");
-//  Serial.print(foundPuckAngle);
-//  Serial.print("   seesPuck: ");
-//  Serial.print(seesPuck);
-//  Serial.print("   leftScanEdge: ");
-//  Serial.print(leftScanEdge);
-//  Serial.print("   rightScanEdge: ");
-//  Serial.print(rightScanEdge);
-//  Serial.print("   scanDirection: ");
-//  Serial.print(scanDirection);
- //Serial.print("   scanServoAngle: ");
- // Serial.println(scanServoAngle);
- 
- Serial.print("\n");
+  scanForPuck();
   
-  //delay(100);		             // Short delay
+  int speedLeft = 0;
+  int speedRight = 0;
+  
+  if (foundPuck) {
+    
+    if (foundPuckStraightAheadTime > 4) {
+      speedLeft += 50;
+      speedRight += 50;
+    } else {
+      int dir = -(float)foundPuckAngle * 0.4f; // scale puck angle to make it more suitable for driving the motors
+      speedLeft -= dir;
+      speedRight += dir;
+    }
+  }
+  
+  servoLeft.writeMicroseconds(1500 - speedLeft);
+  servoRight.writeMicroseconds(1500 + speedRight);
+  
+  //Serial.print("\n");
 }
 
 unsigned long checkSonar(int pingPin) {
-  digitalWrite(pingPin, HIGH);       // Start ranging
-  delayMicroseconds(5);              // with 5 microsecond burst
-  digitalWrite(pingPin, LOW);        // End ranging
+  // Start ranging
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
   
+  // Wait for the result
   pinMode(pingPin, INPUT);
   unsigned long duration = pulseIn(pingPin, HIGH, (unsigned long)3000);//5772); // Read echo pulse
-  unsigned long cm;
+  delay(sonarReadDelay); // Make sure that we wait to let the sound die out so that it doesn't fool any other sensor
   
   if (duration == 0) {
     duration = 3000;
   }
   
   unsigned long inches = duration / 148;
-  cm = inches * 2.54;
+  unsigned long cm = inches * 2.54;
   
+  //Serial.print(pingPin);
+  //Serial.print(" cm: ");
+  //Serial.print(cm);
+  //Serial.print("\t");
   
-  pinMode(pingPin, OUTPUT); // Set pin to OUTPUT
-  digitalWrite(pingPin, LOW); // Ensure pin is low
-  
-  Serial.print(pingPin);
-  Serial.print(" cm: ");
-  Serial.print(cm);
-  Serial.print("\t");
+  // Make sure that we reset the pin before leaving the function
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
   
   return cm;
 }
 
 /* Will scan for pucks and walls. 
 * this function is communicating it's result by setting some global variables:
-* boolean foundPuck, high = is found
-* int foundPuckDist, the distance to the Puck in cm
-* int foundPuckAngle, the angle in degree where the puck is, from -90 to + 90 deg.
-* boolean foundWall, if we see any wall.
-* int foundWallAngle, the angle to the closest found wall, from -90 to + 90 deg.
-* int foundWallDist, the distance to the closest found wall in cm.
+* ...
 */
-void scanForPuck(int lowSensorPin, int highSensorPin){
-  boolean sawPuck = seesPuck;   
-  unsigned long lowDist = checkSonar(lowSensorPin);
-  delay(50);
-  unsigned long highDist = checkSonar(highSensorPin);
-  delay(50);
-  int threshold = 10;
+void scanForPuck() {
+  unsigned long lowDist = checkSonar(pingPinLow);
+  unsigned long highDist = checkSonar(pingPinHigh);
   
-  seesPuck = (((lowDist + threshold) < highDist) && (lowDist < 60));// do we see a puck?
-
-  if(!foundPuck){
-    if(seesPuck){//just found the puck(for the "first time")
-      leftScanEdge = scanServoAngle;
-      rightScanEdge = scanServoAngle;
-      foundPuck= true;
-    }
-    if(scanServoAngle >= scanRange){ //too much left
-      scanDirection= !scanDirection;
-      dScanAngle= -abs(dScanAngle);
-    }
-    if(scanServoAngle <= -scanRange){ // too much right
-      scanDirection= !scanDirection;
-      dScanAngle= abs(dScanAngle);
-    }
-  }else{ // foundPuck
-    if(seesPuck){ 
-      if(sawPuck){ // foundPuck & see & saw
-       if(scanServoAngle > leftScanEdge){
-         leftScanEdge = scanServoAngle;
-       }
-       if(scanServoAngle < rightScanEdge){
-         rightScanEdge = scanServoAngle;
-       }
-      }else{ // foundPuck & see & NOT saw
-        if(scanDirection){ //we are going right
-          leftScanEdge = scanServoAngle;
-        }else{
-          rightScanEdge = scanServoAngle;            
-        }
-      }  
-    }else{ //don't see the puck
-      if(sawPuck){// foundPuck & NOT see & saw
-        scanDirection= !scanDirection;
-        dScanAngle= -dScanAngle; 
-      }else{// foundPuck & NOT see & NOT saw
-        if(abs(scanServoAngle) >= scanRange){
-          foundPuck = false;
-            scanDirection= !scanDirection;
-            dScanAngle= -dScanAngle;
-        }
-      }
-      
-    }
+  // Only look for a puck once during each sweep. Makes the check super robust!
+  if (!sawThisSweep) {
+    // Update puck observation
+    sawPuckLastCheck = seesPuck;
+    seesPuck = ((lowDist + sonarThreshold < highDist) && (lowDist < sonarRange)); // do we see a puck?
     
-    if(abs(scanServoAngle) >= scanRange){
-      scanDirection= !scanDirection;
-      dScanAngle= -dScanAngle;
+    if (seesPuck && !sawPuckLastCheck) {
+      puckStartAngle = scanServoAngle;
+    }
+    else if (!seesPuck && sawPuckLastCheck) {
+      puckEndAngle = scanServoAngle;
+      sawThisSweep = true;
     }
   }
   
+  // Rotate sensor
+  scanServoAngle += scanServoAngleDelta;
   
-  foundPuckAngle =  (leftScanEdge + rightScanEdge)/2;
+  if (abs(scanServoAngle) > scanServoAngleLimit) {
+    // Sweep ended
+    foundPuck = sawThisSweep;
     
-  //rotate sensor
-  scanServoAngle += dScanAngle;
-  servoScanner.writeMicroseconds(1500 + scanServoAngle); // Set right servo speed
+    if (foundPuck) {
+      foundPuckAngle = (puckStartAngle + puckEndAngle) / 2;
+      
+      // Keep track of for how many sweeps the puck is straight ahead
+      if (abs(foundPuckAngle) < 20) {
+        foundPuckStraightAheadTime++;
+        tone(2, 3000, 100);
+      } else {
+        foundPuckStraightAheadTime = 0;
+      }
+    }
+    
+    // Start new sweep and change direction
+    sawThisSweep = false;
+    seesPuck = false;
+    sawPuckLastCheck = false;
+    puckStartAngle = 0;
+    puckEndAngle = 0;
+    scanServoAngleDelta = -scanServoAngleDelta;
+  }
+  
+  int servoTiming = 1500 + ((long)(scanServoAngleOffset + scanServoAngle) * 820) / 90;
+  servoScanner.writeMicroseconds(servoTiming);
 }
