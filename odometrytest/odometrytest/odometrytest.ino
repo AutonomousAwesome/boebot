87484cd798ba92ff7f65cc94c42a53ae68bf87a3
mikaeltulldahl <mikael.tulldahl@live.se>
2015-05-19 21:45:18 +0200

    clean up odometerTest

--- odometrytest/odometrytest/odometrytest.ino
+++ odometrytest/odometrytest/odometrytest.ino
@@ -7,18 +7,19 @@
 
 Servo servoLeft;
 Servo servoRight;
-
+//odometry
 float heading = 0;
-
 float posX = 0;
 float posY = 0;
+
 int state = 2;
 
 long startTime = 0;
-long loopPeriod_us = 10000; // in microseconds
-float loopPeriod_s = loopPeriod_us / 1000000;
+long loopPeriod = 10000; // in microseconds
+float loopPeriod_s = loopPeriod / 1000000;
 
-
+int speedLeft = 0;
+int speedRight = 0;
 
 void setup()                                 // Built-in initialization block
 {
@@ -30,71 +31,47 @@ void setup()                                 // Built-in initialization block
 }
 
 void loop() { // Main loop auto-repeats
-
-   while(startTime > micros()) ; // wait here until we get constant looptime
-   startTime = micros() + loopPeriod_us;
-//Serial.println(micros());
-Serial.println(heading);
-
-
-
-  //make decision(s)
+  while(startTime > micros()) ; // wait here until we get constant looptime
+  startTime = micros() + loopPeriod;
+  odometry(speedRight, speedLeft);
+   
+  Serial.println(heading);
+   
   switch (state) {
     case 2: // left turn
-    if (heading > 360){
-      state = 3;
-      tone(4, 3000, 1000);
+      speedLeft = -30;
+      speedRight = 30;
+      if (heading > 180){
+        state = 3;
+        speedLeft = 0;
+        speedRight = 0;
+        tone(4, 3000, 1000);
       }
       break;
     case 3: // stop
+      speedLeft = 0;
+      speedRight = 0;
       break;
   }
-      //take action
-      int speedLeft = 0;
-      int speedRight = 0;
-
-      switch (state) {
-        case 0: // forward
-          speedLeft = 100;
-          speedRight = 100;
-          break;
-        case 1: // right turn
-          speedLeft = 30;
-          speedRight = -30;
-          break;
-        case 2: // left turn
-          speedLeft = -30;
-          speedRight = 30;
-          break;
-        case 3: // stop & beep
-          speedLeft = 0;
-          speedRight = 0;
-          break;
-      }      
-      odometry(speedRight, speedLeft, loopPeriod_us);
-      maneuver(speedLeft,speedRight);
-    }
+  drive();
+}
     
-void odometry(int speedRight, int speedLeft, long loopPeriod_us){
-  //TODO compensate for nonlinearity
-  heading += (speedRight - speedLeft)*(loopPeriod_us/(float)1000000)*(0.82);
-  //if( heading < 0) heading +=360;
-  //if( heading >360) heading -=360;
+void odometry(int speedRight, int speedLeft){
+  heading += (speedRight - speedLeft)*(loopPeriod/(float)1000000)*(0.82);
   
-  //TODO finc good constant
+  while( heading < 0){
+    heading +=360;
+  }
+  while( heading >360){
+    heading -=360;
+  }
+  
   int speed = 0.1*(speedLeft + speedRight)/2;
-  posX += cos(heading*pi/180)*speed*loopPeriod_s;
-  posY += sin(heading*pi/180)*speed*loopPeriod_s;
+  posX += cos(heading*3.14/180)*speed*loopPeriod_s;
+  posY += sin(heading*3.14/180)*speed*loopPeriod_s;
 }
 
-      void maneuver(int speedLeft, int speedRight)
-      {
-        servoLeft.writeMicroseconds(1500 + speedLeft);   // Set left servo speed
-        servoRight.writeMicroseconds(1500 - speedRight); // Set right servo speed
-        //if (msTime == -1)                                // if msTime = -1
-        //{
-        //  servoLeft.detach();                            // Stop servo signals
-        //  servoRight.detach();
-        //}
-      
-      }
+void drive(){
+  servoLeft.writeMicroseconds(1500 - speedLeft);   // Set left servo speed
+  servoRight.writeMicroseconds(1500 + speedRight); // Set right servo speed
+}
